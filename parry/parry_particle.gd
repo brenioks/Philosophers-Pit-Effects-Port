6643917034522d9787622b69aa531da762686de2
brenio <72355208+brenioks@users.noreply.github.com>
2026-01-15 16:45:48 -0300

    Parry freeze frames

--- parry/parry_particle.gd
+++ parry/parry_particle.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var particles: CPUParticles2D = $CPUParticles2D
+@onready var unfreeze_timer: Timer = $UnfreezeTimer
 
 
 func _ready() -> void:
@@ -10,5 +11,10 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.is_action_pressed("parry"):
-			particles.emitting = true
-			
+			particles.restart()
+			particles.speed_scale = 0
+			unfreeze_timer.start()
+
+
+func _unfreeze_particles():
+	particles.speed_scale = 1
