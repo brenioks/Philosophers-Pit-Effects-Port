e8d6dba6c5cd18aefb3b4a08820def0444f553d9
brenio <72355208+brenioks@users.noreply.github.com>
2026-01-15 17:49:16 -0300

    Method to start parry
    - added method to do the parry, cuz thas better;
    - added export button as helper for Editor;

--- parry/parry_particle.gd
+++ parry/parry_particle.gd
@@ -1,4 +1,7 @@
+@tool
 extends Node2D
+
+@export_tool_button("Do the parry") var start_parry_action = start_parry
 
 @onready var particles: CPUParticles2D = $CPUParticles2D
 @onready var unfreeze_timer: Timer = $UnfreezeTimer
@@ -11,12 +14,22 @@ func _ready() -> void:
 	flashbang.visible = false
 
 func _input(event: InputEvent) -> void:
+	if Engine.is_editor_hint():
+		return
+	
 	if event is InputEventKey:
 		if event.is_action_pressed("parry"):
-			particles.restart()
-			particles.speed_scale = 0
-			flashbang.visible = true
-			unfreeze_timer.start()
+			start_parry()
+
+func start_parry():
+	particles.restart()
+	particles.speed_scale = 0
+	flashbang.visible = true
+	if Engine.is_editor_hint():
+		var rect: ColorRect = flashbang.get_node("ColorRect")
+		rect.global_position = global_position - rect.size * .5
+	
+	unfreeze_timer.start()
 
 func unfreeze():
 	particles.speed_scale = 1
