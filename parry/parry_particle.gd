f9c7df9a32f69bddccecf5a1e769416004c1fe4d
brenio <72355208+brenioks@users.noreply.github.com>
2026-01-15 17:16:01 -0300

    Added a white screen when parrying
    the Flashbang CanvasLayer

--- parry/parry_particle.gd
+++ parry/parry_particle.gd
@@ -2,19 +2,22 @@ extends Node2D
 
 @onready var particles: CPUParticles2D = $CPUParticles2D
 @onready var unfreeze_timer: Timer = $UnfreezeTimer
+@onready var flashbang: CanvasLayer = $Flashbang
 
 
 func _ready() -> void:
 	particles.one_shot = true
 	particles.emitting = false
+	flashbang.visible = false
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.is_action_pressed("parry"):
 			particles.restart()
 			particles.speed_scale = 0
+			flashbang.visible = true
 			unfreeze_timer.start()
 
-
-func _unfreeze_particles():
+func unfreeze():
 	particles.speed_scale = 1
+	flashbang.visible = false
